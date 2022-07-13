5eb2fa8d1fdef3a816f84737f46545a33ede5d67
andrea <andreablass11@gmail.com>
2022-07-13 13:10:08 -0500

    User auth token

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,36 +1,70 @@
 class PostsController < ApplicationController
-    #to exception handling, rescue
-    rescue_from Exception do |e|
-      render json: {error: e.message}, status: :internal_error
+  before_action :authenticate_user!, only: [:create, :update]
+
+  rescue_from Exception do |e|
+    render json: {error: e.message}, status: :internal_error
+  end
+
+  rescue_from ActiveRecord::RecordInvalid do |e|
+    render json: {error: e.message}, status: :unprocessable_entity
+  end
+
+  # GET /posts
+  def index
+    @posts = Post.where(published: true)
+    if !params[:search].nil? && params[:search].present?
+      @posts = PostsSearchService.search(@posts, params[:search])
     end
-  
-    rescue_from ActiveRecord::RecordInvalid do |e|
-      render json: {error: e.message}, status: :unprocessable_entity
-    end
-  
-    # GET /posts
-    def index
-      @posts = Post.where(published: true)
-      render json: @posts, status: :ok
-    end
-  
-    # GET /posts/{id}
-    def show
-      @post = Post.find(params[:id])
+    render json: @posts.includes(:user), status: :ok
+  end
+
+  # GET /posts/{id}
+  def show
+    @post = Post.find(params[:id])
+    if(@post.published? || (Current.user && @post.user_id == Curen.user.id))
       render json: @post, status: :ok
-    end
-  
-    # POST /posts
-    def create
-      @post = Post.create!(create_params)
-      render json: @post, status: :created
-    end
-  
-    # PUT /posts/{id}
-    def update
-      @post = Post.find(params[:id])
-      @post.update!(update_params)
-      render json: @post, status: :ok
+    else
+      render json: {error: 'Not Found'}, status: :not_found
     end
   end
-  
+
+  # POST /posts
+  def create
+    @post = Current.user.posts.create!(create_params)
+    render json: @post, status: :created
+  end
+
+  # PUT /posts/{id}
+  def update
+    @post = Current.user.posts.find(params[:id])
+    @post.update!(update_params)
+    render json: @post, status: :ok
+  end
+
+  private
+
+  def create_params
+    params.require(:post).permit(:title, :content, :published)
+  end
+
+  def update_params
+    params.require(:post).permit(:title, :content, :published)
+  end
+
+  def authenticate_user!
+    # Bearer xxxxxx
+    token_regex = /Bearer (\w+)/
+    # leer HEADER de auth
+    headers = request.headers
+    # verificar que sea valido
+    if headers['Authorization'].present? && headers['Authorization'].match(token_regex)
+      token = headers['Authorization'].match(token_regex)[1]
+      # debemos verificar que el token corresponda a un user
+      if(Current.user = User.find_by_auth_token(token))
+        return
+      end
+    end
+
+    render json: {error: 'Unauthorized'}, status: :unauthorized
+  end
+end
